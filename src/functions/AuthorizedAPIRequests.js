e06331f721e5efc35d45bee7e27441cfd3f2465d
Patrick <patrick.ziajski@gmail.com>
2021-04-03 17:50:11 -0400

    set to pass cookies

--- src/functions/AuthorizedAPIRequests.js
+++ src/functions/AuthorizedAPIRequests.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
+const HTTP = axios.create({
+    withCredentials: true
+})
+
 export const authGetRequest = (endpoint) => {
-    return axios.get(`${process.env.REACT_APP_API}/${endpoint}`, {
-        withCredentials: true
-    });
+    return HTTP.get(`${process.env.REACT_APP_API}/${endpoint}`);
 };
 
 export const authPostRequest = (endpoint, data) => {
-    return axios.post(`${process.env.REACT_APP_API}/${endpoint}`, data, {
-        withCredentials: true
-    });
+    return HTTP.post(`${process.env.REACT_APP_API}/${endpoint}`, data);
 };
