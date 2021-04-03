53049e3fea44aa85990eb73009a8c242997b6297
Patrick <patrick.ziajski@gmail.com>
2021-04-03 19:17:59 -0400

    moved viewChannel and set to update with url params

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,23 +4,47 @@ import { authGetRequest, authPostRequest } from "../../functions/AuthorizedAPIRe
 import "./Chat.scss";
 
 export const Chat = (props) => {
-    const { userInfo, channelDetails } = props;
+    const { userInfo } = props;
     const channelId = props.match.params.channelId;
-    const [channelMessages, setChannelMessages] = useState([]);
 
-    // get channel messages
-    const getChannelMessages = (isMounted) => {
-        authGetRequest(`chats/${channelId}`)
+    const [viewChannel, setViewChannel] = useState(props.match.params.channelId ? props.match.params.channelId : 1);
+    useEffect(() => {
+        setViewChannel(props.match.params.channelId);
+    }, [props.match.params.channelId])
+
+    const [channelDetails, setChannelDetails] = useState([]);
+    useEffect(() => {
+        let isMounted = true;
+        authGetRequest(`channels/${viewChannel}`)
             .then(response => {
                 if (isMounted) {
-                    setChannelMessages(response.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)));
+                    setChannelDetails(response.data);
                 }
             })
             .catch(error => {
                 console.error(error);
             })
-    }
 
+        return () => {
+            isMounted = false;
+        }
+    }, [viewChannel]);
+
+    
+    // get channel messages
+    const getChannelMessages = (isMounted) => {
+        authGetRequest(`chats/${channelId}`)
+        .then(response => {
+            if (isMounted) {
+                setChannelMessages(response.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)));
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        })
+    }
+    
+    const [channelMessages, setChannelMessages] = useState([]);
     useEffect(() => {
         let isMounted = true;
         getChannelMessages(isMounted);
