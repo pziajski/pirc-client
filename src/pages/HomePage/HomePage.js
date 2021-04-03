2bba8f2702ff642595038a474e070deb57c09423
Patrick <patrick.ziajski@gmail.com>
2021-04-03 18:49:39 -0400

    code reformat

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import { Chat } from "../../components/Chat/Chat";
 import { Channels } from "../../components/Channels/Channels";
 import { UserSettings } from "../../components/UserSettings/UserSettings";
-import { authGetRequest, authPostRequest } from "../../functions/AuthorizedAPIRequests";
+import { authGetRequest } from "../../functions/AuthorizedAPIRequests";
 import "./HomePage.scss";
 
 export const HomePage = (props) => {
@@ -12,7 +12,6 @@ export const HomePage = (props) => {
         let isMounted = true;
         authGetRequest(`users/userInfo`)
             .then(response => {
-                console.log("GET USERS INFO ->>", response)
                 if (isMounted) {
                     setUserInfo(response.data);
                 }
@@ -36,6 +35,9 @@ export const HomePage = (props) => {
                         setUserChannelsJoined(response.data);
                     }
                 })
+                .catch(error => {
+                    console.error(error);
+                })
         }
 
         return () => {
@@ -58,71 +60,22 @@ export const HomePage = (props) => {
                     setChannelDetails(response.data);
                 }
             })
-
-        return () => {
-            isMounted = false;
-        }
-    }, [viewChannel]);
-
-    // get channel messages
-    const getChannelMessages = (isMounted) => {
-        authGetRequest(`chats/${viewChannel}`)
-            .then(response => {
-                if (isMounted) {
-                    setChannelMessages(response.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)));
-                }
-            })
-    }
-
-    const [channelMessages, setChannelMessages] = useState([]);
-    useEffect(() => {
-        let isMounted = true;
-        getChannelMessages(isMounted);
-
-        return () => {
-            isMounted = false;
-        }
-    }, [viewChannel]);
-
-    // get messages at an interval
-    useEffect(() => {
-        let isMounted = true;
-        const getMessagesOnInterval = setInterval(() => {
-            getChannelMessages(isMounted);
-        }, 1000);
-        return () => {
-            isMounted = false;
-            clearInterval(getMessagesOnInterval);
-        }
-    }, [])
-
-    const sendMessage = (messageData) => {
-        const { channelId, user_id, message } = messageData;
-        const data = {
-            user_id,
-            message
-        };
-
-        authPostRequest(`chats/${channelId}`, data)
-            .then(response => {
-                const newMessage = {
-                    ...response.data,
-                    user: {
-                        ...userInfo
-                    }
-                };
-                setChannelMessages([newMessage, ...channelMessages]);
-            })
             .catch(error => {
-                console.error("...ERROR... failed to send message sendMessage ->", error);
+                console.error(error);
             })
-    }
+
+        return () => {
+            isMounted = false;
+        }
+    }, [viewChannel]);
+
+
 
     const changeChannels = (channelId) => {
         setViewChannel(channelId);
     }
 
-    if (!userChannelsJoined && !channelMessages && !channelDetails) {
+    if (!userChannelsJoined && !channelDetails) {
         return <>loading...</>
     }
 
@@ -141,8 +94,6 @@ export const HomePage = (props) => {
                     <Chat
                         {...renderProps}
                         userInfo={userInfo}
-                        channelMessages={channelMessages}
-                        sendMessage={sendMessage}
                         channelDetails={channelDetails} />
                 } />
             </Switch>
