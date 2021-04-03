a45094d3f601a00c5a0f7613128619a62a51cf85
Patrick <patrick.ziajski@gmail.com>
2021-04-03 18:49:15 -0400

    moved channelMessages here to rely on match params

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,20 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChatMessage } from "../ChatMessage/ChatMessage";
+import { authGetRequest, authPostRequest } from "../../functions/AuthorizedAPIRequests";
 import "./Chat.scss";
 
 export const Chat = (props) => {
-    const { userInfo, channelMessages, sendMessage, channelDetails } = props;
+    const { userInfo, channelDetails } = props;
     const channelId = props.match.params.channelId;
+    const [channelMessages, setChannelMessages] = useState([]);
 
-    const submitMessage = (e) => {
+    // get channel messages
+    const getChannelMessages = (isMounted) => {
+        authGetRequest(`chats/${channelId}`)
+            .then(response => {
+                if (isMounted) {
+                    setChannelMessages(response.data.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)));
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    useEffect(() => {
+        let isMounted = true;
+        getChannelMessages(isMounted);
+        const getMessagesOnInterval = setInterval(() => {
+            getChannelMessages(isMounted);
+        }, 2000);
+
+        return () => {
+            isMounted = false;
+            clearInterval(getMessagesOnInterval);
+        }
+    }, [channelId]);
+
+    const sendMessage = (e) => {
         e.preventDefault();
-        sendMessage({
-            channelId: channelId,
+
+        const data = {
             user_id: userInfo.id,
             message: e.target.message.value
-        });
+        };
+
         e.target.reset();
+
+        authPostRequest(`chats/${channelId}`, data)
+            .then(response => {
+                const newMessage = {
+                    ...response.data,
+                    user: {
+                        ...userInfo
+                    }
+                };
+                setChannelMessages([newMessage, ...channelMessages]);
+            })
+            .catch(error => {
+                console.error("...ERROR... failed to send message sendMessage ->", error);
+            })
     };
+
+    if (!channelMessages) {
+        return <>Loading...</>
+    }
 
     return (
         <div className="chat">
@@ -27,12 +74,12 @@ export const Chat = (props) => {
                         <li key={message.id} className={message.user_id === userInfo.id ? "chat__message-list-item--personal" : "chat__message-list-item"}>
                             <ChatMessage
                                 userInfo={userInfo}
-                                message={message}/>
+                                message={message} />
                         </li>
                     )
                 }
             </ul>
-            <form className="chat__send-form" onSubmit={submitMessage}>
+            <form className="chat__send-form" onSubmit={sendMessage}>
                 <label className="chat__send-label">
                     <textarea className="chat__send-input" name="message"></textarea>
                 </label>
