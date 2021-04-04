4fb522f330093a81fc67d64b9f0d5bc1add9de53
Patrick <patrick.ziajski@gmail.com>
2021-04-04 19:55:07 -0400

    added encryption to authorized requests

--- src/functions/AuthorizedAPIRequests.js
+++ src/functions/AuthorizedAPIRequests.js
@@ -1,13 +1,22 @@
 import axios from "axios";
+import { decryptData, encryptData } from "./encryption";
 
 const HTTP = axios.create({
     withCredentials: true
 })
 
 export const authGetRequest = (endpoint) => {
-    return HTTP.get(`${process.env.REACT_APP_API}/${endpoint}`);
+    return HTTP.get(`${process.env.REACT_APP_API}/${endpoint}`)
+        .then(response => {
+            return decryptData(response.data.data);
+        });
 };
 
 export const authPostRequest = (endpoint, data) => {
-    return HTTP.post(`${process.env.REACT_APP_API}/${endpoint}`, data);
+    return HTTP.post(`${process.env.REACT_APP_API}/${endpoint}`, {
+        data: encryptData(data)
+    })
+        .then(response => {
+            return decryptData(response.data.data);
+        });
 };
