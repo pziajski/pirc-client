10d6b3f1bc66c594dfc6b37ff11304b639e68ec5
Patrick <patrick.ziajski@gmail.com>
2021-04-05 15:02:22 -0400

    changed overflow to scroll in case there are too many channels

--- src/components/Channels/Channels.js
+++ src/components/Channels/Channels.js
@@ -12,15 +12,17 @@ export const Channels = (props) => {
     return (
         <div className="channels">
             <h2 className="channels__title">{"// Channels"}</h2>
-            <ul className="channels__list">
-                {
-                    userChannelsJoined.map(channel =>
-                        <li className="channels__list-item" key={channel.id}>
-                            <NavLink to={`/channels/${channel.channel.id}`} className="channels__channel" onClick={() => addToSession(channel.channel.id)}>{`< ${channel.channel.name} />`}</NavLink>
-                        </li>
-                    )
-                }
-            </ul>
+            <div className="channels__joined" >
+                <ul>
+                    {
+                        userChannelsJoined.map(channel =>
+                            <li className="channels__list-item" key={channel.id}>
+                                <NavLink to={`/channels/${channel.channel.id}`} className="channels__channel" onClick={() => addToSession(channel.channel.id)}>{`< ${channel.channel.name} />`}</NavLink>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
         </div>
     )
 }
