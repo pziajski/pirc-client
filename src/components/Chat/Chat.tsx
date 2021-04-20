989a2501fb54e0283920c3d514bbac958cf5bc69
Patrick <patrick.ziajski@gmail.com>
2021-04-20 18:35:24 -0400

    complete TS conversion

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import { RouteComponentProps } from "react-router-dom";
 import { ChatMessage } from "../ChatMessage/ChatMessage";
 import { authGetRequest, authPostRequest } from "../../functions/AuthorizedAPIRequests";
 import "./Chat.scss";
+import iUserInfo from '../../interface/iUserInfo';
+import iChannel from '../../interface/iChannel';
+import iMessage from '../../interface/iMessage';
+import iChannelMessages from '../../interface/iChannelMessages';
 
-export const Chat = (props) => {
-    const { userInfo } = props;
-    const channelId = props.match.params.channelId;
+interface iMatchParams {
+    channelId: string
+}
 
+interface iMatchProps extends RouteComponentProps<iMatchParams> {
+    userInfo: iUserInfo
+};
+
+export const Chat: React.FC<iMatchProps> = ({ userInfo, match }) => {
+    const channelId = match.params.channelId;
     const [userInput, setUserInput] = useState("");
+    const viewChannel = channelId;
 
-    const viewChannel = props.match.params.channelId;
-
-    const [channelDetails, setChannelDetails] = useState([]);
+    const [channelDetails, setChannelDetails] = useState<iChannel | {}>({});
     useEffect(() => {
         let isMounted = true;
         authGetRequest(`channels/${viewChannel}`)
-            .then(details => {
+            .then((details: iChannel) => {
                 if (isMounted) {
                     setChannelDetails(details);
                 }
@@ -29,13 +39,13 @@ export const Chat = (props) => {
         }
     }, [viewChannel]);
 
-    const [channelMessages, setChannelMessages] = useState([]);
+    const [channelMessages, setChannelMessages] = useState<iChannelMessages | []>([]);
     useEffect(() => {
         let isMounted = true;
         authGetRequest(`chats/${channelId}`)
             .then(messages => {
                 if (isMounted) {
-                    setChannelMessages(messages.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)));
+                    setChannelMessages(messages.sort((a: iMessage, b: iMessage) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()));
                 }
             })
             .catch(error => {
@@ -45,7 +55,7 @@ export const Chat = (props) => {
             authGetRequest(`chats/${channelId}`)
                 .then(messages => {
                     if (isMounted) {
-                        setChannelMessages(messages.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)));
+                        setChannelMessages(messages.sort((a: iMessage, b: iMessage) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()));
                     }
                 })
                 .catch(error => {
@@ -59,7 +69,8 @@ export const Chat = (props) => {
         }
     }, [channelId]);
 
-    const sendMessage = (e) => {
+    // TODO set exact prop type
+    const sendMessage = (e: any) => {
         e.preventDefault();
         const data = {
             user_id: userInfo.id,
@@ -69,7 +80,7 @@ export const Chat = (props) => {
         setUserInput("");
 
         authPostRequest(`chats/${channelId}`, data)
-            .then(comment => {
+            .then((comment: iMessage) => {
                 setChannelMessages([comment, ...channelMessages]);
             })
             .catch(error => {
@@ -77,7 +88,8 @@ export const Chat = (props) => {
             })
     };
 
-    const updateInputState = (e) => {
+    // TODO set exact prop type
+    const updateInputState = (e: any) => {
         const { target: text } = e;
         setUserInput(text.value);
     }
@@ -89,11 +101,11 @@ export const Chat = (props) => {
     return (
         <div className="chat">
             <div className="chat__channel-header">
-                <h1>{channelDetails.name}</h1>
+                <h1>{(channelDetails as iChannel).name}</h1>
             </div>
             <ul className="chat__message-list">
                 {
-                    channelMessages.map(message =>
+                    channelMessages.map((message: iMessage) =>
                         <li key={message.created_at} className={message.username === userInfo.username ? "chat__message-list-item--personal" : "chat__message-list-item"}>
                             <ChatMessage
                                 userInfo={userInfo}
@@ -102,8 +114,8 @@ export const Chat = (props) => {
                     )
                 }
             </ul>
-            <form className="chat__send-form" onSubmit={sendMessage}>
-                <textarea className="chat__send-input" name="message" maxLength="1000" placeholder="Enter your message here..." value={userInput} onChange={e => updateInputState(e)}></textarea>
+            <form className="chat__send-form" onSubmit={e => sendMessage(e)}>
+                <textarea className="chat__send-input" name="message" maxLength={1000} placeholder="Enter your message here..." value={userInput} onChange={e => updateInputState(e)}></textarea>
                 <button className="chat__send-button" disabled={userInput === "" ? true : false}>Send</button>
             </form>
         </div>
