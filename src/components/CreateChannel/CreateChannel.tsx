8a24ae868a8928eb98a6db2974692983c9da4cb2
Patrick <patrick.ziajski@gmail.com>
2021-04-20 18:36:33 -0400

    complete TS conversion

--- src/components/CreateChannel/CreateChannel.tsx
+++ src/components/CreateChannel/CreateChannel.tsx
@@ -1,10 +1,10 @@
 import React from "react";
+import iCreatChannel from "../../interface/iCreateChannel";
 import "./CreateChannel.scss";
 
-export const CreateChannel = (props) => {
-    const { goBack, createChannel } = props;
-
-    const submitHandler = (e) => {
+export const CreateChannel: React.FC<iCreatChannel> = ({ goBack, createChannel }) => {
+    // TODO set event type
+    const submitHandler = (e: any) => {
         e.preventDefault();
         createChannel(e.target.name.value);
         goBack();
@@ -17,7 +17,7 @@ export const CreateChannel = (props) => {
                 <input className="create-channel__input" type="text" placeholder="Channel Name" name="name" autoComplete="off" required />
                 <div className="create-channel__actions">
                     <button className="create-channel__submit">Submit</button>
-                    <button className="create-channel__cancel" type="click" onClick={goBack}>Cancel</button>
+                    <button className="create-channel__cancel" type="button" onClick={goBack}>Cancel</button>
                 </div>
             </form>
         </div>
